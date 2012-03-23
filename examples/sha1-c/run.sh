990c8af62b54119c0a49178f5db61bf377230b07
Antoine MOYNAULT <antoine.moynault@st.com>
2012-03-23 16:04:30 +0100

    examples: fixed issue with /dev/random, use /dev/urandom.

--- examples/sha1-c/run.sh
+++ examples/sha1-c/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-dd if=/dev/random status=noxfer iflag=fullblock bs=4K count=1K | ./sha1-c
+dd if=/dev/urandom status=noxfer iflag=fullblock bs=4K count=1K | ./sha1-c
 exit ${PIPESTATUS[0]}
 
 

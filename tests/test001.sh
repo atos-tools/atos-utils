75dd9e6130a5d2abe2ebb624990719a6772db60e
Christophe Guillon <christophe.guillon@st.com>
2012-04-25 12:21:58 +0200

    tests: tests now run in their own tmp dir.

--- tests/test001.sh
+++ tests/test001.sh
@@ -6,10 +6,6 @@ source `dirname $0`/common.sh
 
 TEST_CASE="ATOS audit -C option"
 
-rm -rf atos-configurations
-rm -rf atos-configurations-$TEST
-rm -rf atos-configurations/$TEST
-
 $ROOT/atos-audit gcc -o sha1-c  $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c
 [ -f atos-configurations/build.sh ]
 

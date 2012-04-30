498da54d50ed87e0e6fc445545bf6f9a0ddd6aa2
Christophe Guillon <christophe.guillon@st.com>
2012-04-30 13:39:04 +0200

    contrib/gerrit: added push-change/update-change scripts for gerrit review.

---
+++ contrib/gerrit/config.sh
@@ -0,0 +1,9 @@
+#
+# Default configuration for gerrit reviews.
+# Can be overriden by a local.sh file in the same directory.
+#
+server=ssh://hostname:29418
+project=atos
+branch=master
+reviewers=
+remote=gerrit

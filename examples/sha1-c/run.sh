a04ac24a633be42d14acbbb475083cdf88dd29e1
Antoine MOYNAULT <antoine.moynault@st.com>
2012-05-14 19:18:56 +0200

    atos-explore: fixed -C option handling.

--- examples/sha1-c/run.sh
+++ examples/sha1-c/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-dd if=/dev/urandom status=noxfer iflag=fullblock bs=4K count=1K | ./sha1-c
+dd if=/dev/urandom status=noxfer iflag=fullblock bs=4K count=1K 2>/dev/null | ./sha1-c
 exit ${PIPESTATUS[0]}
 
 

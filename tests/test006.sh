253ee000759dfb814966be037859ba22017f8ca4
Antoine MOYNAULT <antoine.moynault@st.com>
2012-05-14 19:17:54 +0200

    atos-deps: fixed -C option handling.

---
+++ tests/test006.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS deps -C option"
+
+$ROOT/atos-audit gcc -o sha1-c  $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c
+$ROOT/atos-deps -a
+[ -f atos-configurations/build.mk ]
+
+$ROOT/atos-audit -C atos-configurations-1 gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c
+$ROOT/atos-deps -a -C atos-configurations-1
+[ -f atos-configurations-1/build.mk -a -f atos-configurations-1/targets ]
+[ `diff atos-configurations-1/build.mk atos-configurations/build.mk | wc -l` -eq 0 ]
+
+mkdir atos-configurations-2
+$ROOT/atos-deps -a -C atos-configurations-2 -i atos-configurations-1/build.audit
+[ -f atos-configurations-2/build.mk -a -f atos-configurations-2/targets ]
+[ `diff atos-configurations-1/build.mk atos-configurations-2/build.mk | wc -l` -eq 0 ]
+
+mkdir atos-configurations-3
+$ROOT/atos-deps -a -C atos-configurations-1 -o atos-configurations-3/build.mk
+[ -f atos-configurations-3/build.mk ]
+[ `diff atos-configurations-1/build.mk atos-configurations-3/build.mk | wc -l` -eq 0 ]

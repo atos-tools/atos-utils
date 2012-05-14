63534302918956bd9e0d44792e44cc2e606522a8
Antoine MOYNAULT <antoine.moynault@st.com>
2012-05-14 19:18:56 +0200

    atos-explore: fixed -C option handling.

---
+++ tests/test008.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS explore -C option"
+
+$ROOT/atos-explore -C atos-configurations-1 -r "$ROOT/examples/sha1-c/run.sh" \
+    -b "gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c"
+    
+[ ! -d atos-configurations ]
+[ `cat ./atos-configurations-1/results.db | grep "time: " | grep -v FAILURE | wc -l` -ne 0 ]

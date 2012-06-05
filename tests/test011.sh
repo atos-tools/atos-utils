a7fc78d8dd04de02e26b5954894a356d9b604558
Christophe Guillon <christophe.guillon@st.com>
2012-06-05 15:40:33 +0200

    tests: fixed some tests not using $ROOT/bin.

--- tests/test011.sh
+++ tests/test011.sh
@@ -8,8 +8,8 @@ TEST_CASE="ATOS deps serial targets"
 
 cp -a $ROOT/examples/sha1 .
 cd sha1
-$ROOT/atos-audit make clean sha-shared
-$ROOT/atos-deps sha.so sha-shared
+$ROOT/bin/atos-audit make clean sha-shared
+$ROOT/bin/atos-deps sha.so sha-shared
 rm *.o *.so
 # Will fail if shared lib (sha.so) is built after executable (sha-shared)
-$ROOT/atos-opt
+$ROOT/bin/atos-opt

ca57c64713e9b036ed9c0d562bd14b0212f1d5c2
Antoine MOYNAULT <antoine.moynault@st.com>
2012-06-13 14:54:13 +0200

    atos-inline/toolkit: added exploration based on existing variants.

---
+++ tests/test021.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS inline variants"
+
+flags_list=""
+
+# temporary fix
+# atos should be able to know compiler version
+
+gcc_version=`gcc --version | head -1 | sed 's/.* //' | sed 's/\.[^.]*$//'`
+if [ "$gcc_version" = "4.4" ]; then
+    flags_list=$ROOT/lib/atos/config/flags.inline.gcc.4.4.cfg
+elif [ "$gcc_version" = "4.5" ]; then
+    flags_list=$ROOT/lib/atos/config/flags.inline.gcc.4.5.cfg
+elif [ "$gcc_version" = "4.6" ]; then
+    flags_list=$ROOT/lib/atos/config/flags.inline.gcc.4.6.cfg
+else
+    skip
+fi
+
+$ROOT/bin/atos-init \
+    -r "$ROOT/examples/sha1-c/run.sh" \
+    -b "gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c"
+
+$ROOT/bin/atos-opt -r -a "-Os"
+
+$ROOT/bin/atos-opt -r -a "-O2"
+
+$ROOT/bin/atos-opt -r -a "-O3"
+
+nb_frontier=`$ROOT/lib/atos/atos_lib.py speedups -C atos-configurations -f | wc -l`
+
+$ROOT/bin/atos-inline -q -M1 -F $flags_list frontier OPT-O2
+
+[ -d atos-configurations ]
+
+# REF + Os + O2 + O3 + inl(front) + inl(O2)
+nb_played=`$ROOT/lib/atos/atos_lib.py query | wc -l`
+
+[ "`expr $nb_frontier + 5`" == "$nb_played" ]
+

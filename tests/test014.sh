18f3a814dc906d16488bcdfcbae9cd7915b1c759
Antoine MOYNAULT <antoine.moynault@st.com>
2012-06-18 15:26:06 +0200

    flag-exploration: added detection of compiler version & features.

--- tests/test014.sh
+++ tests/test014.sh
@@ -6,29 +6,11 @@ source `dirname $0`/common.sh
 
 TEST_CASE="ATOS inline exploration"
 
-flags_list=""
-
-# temporary fix
-# atos should be able to know compiler version
-
-gcc_version=`gcc --version | head -1 | sed 's/.* //' | sed 's/\.[^.]*$//'`
-if [ "$gcc_version" = "4.4" ]; then
-    flags_list=$ROOT/lib/atos/config/flags.inline.gcc.4.4.cfg
-elif [ "$gcc_version" = "4.5" ]; then
-    flags_list=$ROOT/lib/atos/config/flags.inline.gcc.4.5.cfg
-elif [ "$gcc_version" = "4.6" ]; then
-    flags_list=$ROOT/lib/atos/config/flags.inline.gcc.4.6.cfg
-else
-    skip
-fi
-
-(
 $ROOT/bin/atos-init \
     -r "$ROOT/examples/sha1-c/run.sh" \
     -b "gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c" \
 
-$ROOT/bin/atos-explore-inline -M5 -Vbase,fdo -F $flags_list
-) 2>&1
+$ROOT/bin/atos-explore-inline -q -M5 -Vbase,fdo
     
 [ -d atos-configurations ]
 [ `$ROOT/lib/atos/atos_lib.py query -q'variant:OPT-.*' | wc -l` -eq 10 ]

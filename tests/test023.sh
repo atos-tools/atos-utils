9badb715e2e2fc24a2c5b7e590956deb8223f422
Antoine MOYNAULT <antoine.moynault@st.com>
2012-06-18 15:31:49 +0200

    atos-explore-loop: added script for exploration of loop flags.

---
+++ tests/test023.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS inline exploration"
+
+$ROOT/bin/atos-init \
+    -r "$ROOT/examples/sha1-c/run.sh" \
+    -b "gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c" \
+
+$ROOT/bin/atos-explore-loop -q -M5 -Vbase,fdo
+
+[ -d atos-configurations ]
+[ `$ROOT/lib/atos/atos_lib.py query -q'variant:OPT-.*' | wc -l` -eq 10 ]

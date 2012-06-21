3c9436999fb9096319bdb472f7b404c00c7f3fdf
Antoine MOYNAULT <antoine.moynault@st.com>
2012-06-21 11:57:44 +0200

    atos-opt: added -k option for keeping existing results if any.

---
+++ tests/test026.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS opt no-replay"
+
+$ROOT/bin/atos-init \
+    -r "$ROOT/examples/sha1-c/run.sh" \
+    -b "gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c"
+
+$ROOT/bin/atos-opt -r -a "-Os" -k
+
+$ROOT/bin/atos-opt -r -a "-Os" -k
+
+$ROOT/bin/atos-opt -r -a "-O2" -k
+
+$ROOT/bin/atos-opt -r -a "-O2" -k
+
+nb_played=`$ROOT/lib/atos/atos_lib.py query | wc -l`
+
+[ "$nb_played" == "3" ] # REF + Os + O2

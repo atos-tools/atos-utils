030425ede8be981302c8776c3488e7772c32ace3
Christophe Guillon <christophe.guillon@st.com>
2012-06-22 18:32:07 +0200

    atos-deps: added generation of objects list.
    The list of generated object files, i.e. target of a build
    command is dumped into atos-configurations/objects.

--- tests/test004.sh
+++ tests/test004.sh
@@ -9,9 +9,12 @@ TEST_CASE="ATOS deps target/-l/-a"
 $ROOT/bin/atos-audit gcc -o sha1-c  $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c
 $ROOT/bin/atos-deps sha1-c
 [ -f atos-configurations/targets ]
+[ -f atos-configurations/objects ]
 [ -f atos-configurations/build.mk ]
 targets=`cat atos-configurations/targets`
 [ "$targets" = $PWD/sha1-c ]
+objects=`cat atos-configurations/objects`
+[ "$objects" = "" ] # no objects as it is a link command
 mv sha1-c sha1-c.ref
 make -f atos-configurations/build.mk
 cmp sha1-c sha1-c.ref
@@ -25,6 +28,10 @@ cd sha1
 make clean
 $ROOT/bin/atos-audit make all
 $ROOT/bin/atos-deps -a
+targets=`cat atos-configurations/targets`
+[ "$targets" != "" ] # check that some targets was sen
+objects=`cat atos-configurations/objects`
+[ "$objects" != "" ] # check that some objects was sen
 mv sha sha.ref
 mv shacmp shacmp.ref
 mv shatest shatest.ref

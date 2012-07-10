86021ffb4b4773c6ecf5058bb486b9538345a2aa
Antoine MOYNAULT <antoine.moynault@st.com>
2012-07-10 15:54:57 +0200

    atos-play: fixed issues on new dbs by using atos_lib library.

---
+++ tests/test033.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS play options"
+
+$ROOT/bin/atos-init \
+    -r "$ROOT/examples/sha1-c/run.sh" \
+    -b "gcc -o sha1-c $ROOT/examples/sha1-c/sha.c $ROOT/examples/sha1-c/sha1.c"
+
+$ROOT/bin/atos-opt -r -a "-O2"
+
+$ROOT/bin/atos-opt -r -a "-O3"
+
+$ROOT/bin/atos-play -p -e sha1-c
+
+$ROOT/bin/atos-play -p -i sha1-c
+
+$ROOT/bin/atos-play -n -f size
+
+[ `$ROOT/bin/atos-play -p -r | grep "ATOS: sha1-c: REF" | wc -l` -ne 0 ]
+
+[ `$ROOT/bin/atos-play -p -l d82cd2 | grep "ATOS: sha1-c: REF" | wc -l` -ne 0 ]
+

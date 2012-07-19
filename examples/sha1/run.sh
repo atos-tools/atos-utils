d3009caea8134aa94e429ffd10a3346fd636ad37
Christophe Guillon <christophe.guillon@st.com>
2012-07-19 19:42:11 +0200

    examples: fixed run script.
    The dd iflag=fullblock option is not recognized on some systems.
    Removed it.

--- examples/sha1/run.sh
+++ examples/sha1/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-dd if=/dev/urandom status=noxfer iflag=fullblock bs=4K count=1K 2>/dev/null | ./sha
+dd if=/dev/urandom status=noxfer bs=4K count=1K 2>/dev/null | ./sha
 exit ${PIPESTATUS[0]}
 
 

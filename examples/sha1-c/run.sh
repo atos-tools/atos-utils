626d98595b5b7c70d89c3c5b2a438823d1fb7b39
Antoine MOYNAULT <antoine.moynault@st.com>
2012-07-20 17:50:00 +0200

    examples: fixed issue with run script.
    removed status=noxfer option, unavailable on some machines.

--- examples/sha1-c/run.sh
+++ examples/sha1-c/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-dd if=/dev/urandom status=noxfer bs=4K count=1K 2>/dev/null | ./sha1-c
+dd if=/dev/urandom bs=4K count=1K 2>/dev/null | ./sha1-c
 exit ${PIPESTATUS[0]}
 
 

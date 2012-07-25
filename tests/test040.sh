cb3d9efa17abc36433730dd6d08e6a07a6507383
Antoine MOYNAULT <antoine.moynault@st.com>
2012-07-25 10:22:56 +0200

    atos-explore: fixed issue on empty flag list.

---
+++ tests/test040.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS empty flag list"
+
+$ROOT/bin/atos-init \
+    -r "$SRCDIR/examples/sha1-c/run.sh" \
+    -b "gcc -o sha1-c $SRCDIR/examples/sha1-c/sha.c $SRCDIR/examples/sha1-c/sha1.c"
+
+touch f.txt
+
+$ROOT/bin/atos-explore-inline -F ./f.txt -M 5
+
+[ `$ROOT/lib/atos/atos_lib.py query | grep -v REF | wc -l` -eq 0 ]

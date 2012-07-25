124b739ed185ecdcfdb0b28d1e3debf611c971cf
Antoine MOYNAULT <antoine.moynault@st.com>
2012-07-25 14:51:20 +0200

    examples: fixed qemu run scripts.

--- examples/sha1/run_qemu.sh
+++ examples/sha1/run_qemu.sh
@@ -6,7 +6,7 @@ case arch in
     i386|i586|i686) arch=i386
 	;;
 esac
-dd if=/dev/urandom status=noxfer iflag=fullblock bs=4K count=1K 2>/dev/null | qemu-$arch -count-ifetch ./sha 2>$tmpfile
+dd if=/dev/urandom bs=4K count=1K 2>/dev/null | qemu-$arch -count-ifetch ./sha 2>$tmpfile
 [ ${PIPESTATUS[0]} = 0 ] || exit 1
 awk '{if ($1 == "qemu:") { print "user " $10 / 1000000000; }}' <$tmpfile
 

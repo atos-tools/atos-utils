f6a1db59fa4832a1a93c9c9f2346adf3b3b1aa20
Christophe Guillon <christophe.guillon@st.com>
2012-09-04 14:25:57 +0200

    tests: removed some KEEPTEST and fixed cleaning of tmpdirs.
    Also, minor change in output of makefile.

--- tests/common.sh
+++ tests/common.sh
@@ -5,11 +5,13 @@ ROOT=${ROOT:-$SRCDIR}
 TEST=`basename $0 .sh`
 TMPDIR=${TMPDIR:-/tmp}
 KEEPTEST=${KEEPTEST:-0}
+_skipped=0
 
 cleanup() {
     local exit=$?
+    cd $TMPDIR # ensure not in TMPTEST before cleaning
     [ ! -d "$TMPTEST" -o "$KEEPTEST" != 0 ] || rm -rf $TMPTEST
-    [ $exit != 0 ] || success
+    [ $exit != 0 -o $_skipped = 1 ] || success
     [ $exit = 0 -o $exit -ge 128 ] || failure
     [ $exit = 0 -o $exit -lt 128 ] || interrupted && trap - EXIT && exit $exit
 }
@@ -32,7 +34,8 @@ success() {
 skip() {
     local reason=${1+": $1"}
     echo "---SKIP: $TEST: $TEST_CASE$reason" >&2
-    trap - EXIT && exit 0
+    _skipped=1
+    exit 0
 }
 
 [ "$DEBUG" = "" ] || set -x

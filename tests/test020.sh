a7ad9d8610118d7c50ff39099cbb4672bbfd7111
Antoine MOYNAULT <antoine.moynault@st.com>
2012-09-21 19:53:52 +0200

    atos-replay: rewrote atos-play in python.

--- tests/test020.sh
+++ tests/test020.sh
@@ -5,7 +5,7 @@
 source `dirname $0`/common.sh
 
 TEST_CASE="ATOS replay frontier"
-skip "test skipped until atos-replay rewriting"
+
 $ROOT/bin/atos-init \
     -r "$SRCDIR/examples/sha1-c/run.sh" \
     -b "gcc -o sha1-c $SRCDIR/examples/sha1-c/sha.c $SRCDIR/examples/sha1-c/sha1.c"

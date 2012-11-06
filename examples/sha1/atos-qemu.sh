745e53fbf1d0a527f44a9ca313d6e497e30078ad
Christophe Guillon <christophe.guillon@st.com>
2012-11-06 18:55:42 +0100

    examples/sha1: added missing atos-qemu.sh script.

---
+++ examples/sha1/atos-qemu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+arch=`uname -m`
+case $arch in
+    i486|i586|i686) arch=i386
+	;;
+esac
+
+QEMU=${QEMU:-qemu-$arch}
+TCG_PLUGIN=${QEMU_PLUGIN+"-tcg-plugin ${QEMU_PLUGIN}"}
+QEMU_INST="$QEMU -count-ifetch ${TCG_PLUGIN}"
+
+cleanup() {
+    local code=$?
+    trap - EXIT
+    [ ! -f $tmpfile ] || rm -f $tmpfile
+    exit $code
+}
+trap cleanup INT QUIT TERM EXIT
+tmpfile=`mktemp /tmp/run.XXXXXX`
+
+${QEMU_INST} "$@" 2>$tmpfile
+code=$?
+cat <$tmpfile >&2
+awk '{if ($1 == "qemu:") { print "user " $10 / 1000000000; }}' <$tmpfile >&2
+exit $code

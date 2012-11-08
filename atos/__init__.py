6ae3b79895b9401cc02edd699eed750c48e30173
Antoine MOYNAULT <antoine.moynault@st.com>
2012-11-08 17:31:38 +0100

    coverage: added no cover pragma on wrong python version exception.

--- atos/__init__.py
+++ atos/__init__.py
@@ -21,7 +21,7 @@ import sys, os, signal
 def check_python_version_():
     try:
         assert sys.hexversion >= 0x02060000
-    except:
+    except:  # pragma: no cover
         print >>sys.stderr, \
             'error: python version >= 2.6 is required by ATOS tools'
         sys.exit(1)

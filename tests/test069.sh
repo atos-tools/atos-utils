5bf4edd672a2f97dde651f88159bc54b7096b672
Antoine MOYNAULT <antoine.moynault@st.com>
2013-04-02 18:10:49 +0200

    generators: moved unit tests out from sources.

--- tests/test069.sh
+++ tests/test069.sh
@@ -13,8 +13,6 @@ python $ROOT/lib/atos/python/atoslib/deep_eq.py -v
 
 python $ROOT/lib/atos/python/atoslib/cmd_interpreter.py
 
-python $ROOT/lib/atos/python/atoslib/generators.py
-
 python $ROOT/lib/atos/python/atoslib/graphs.py
 
 python $ROOT/lib/atos/python/atoslib/atos_argparse.py

6da078e48d32ff813c9884b13ec76e6235258082
Antoine MOYNAULT <antoine.moynault@st.com>
2013-04-09 17:49:05 +0200

    generators: added gen_explore_random exploration.

--- tests/test090.sh
+++ tests/test090.sh
@@ -19,17 +19,32 @@ $ROOT/bin/atos-init \
 
 # chain gen_basic, gen_inline and gen_loop
 $ROOT/bin/atos generator \
-    --generator=gen_chained_exploration \
-    --extra-arg=generator1=gen_basic \
-    --extra-arg=generator1=gen_explore_inline \
-    --extra-arg=generator1=gen_explore_loop \
+    --generator=gen_chained \
+    --extra-arg=generator1=gen_base \
+    --extra-arg=generator2=gen_explore_inline \
+    --extra-arg=generator3=gen_explore_loop \
     --nbiters=5 --optim-levels=-O3 --optim-variants=base,lto --cookie=expl_first
 
 # continue previous exploration with gen_optim
 $ROOT/bin/atos generator \
     --generator=gen_chained_exploration \
-    --extra-arg=generator1=gen_rnd_uniform_deps,flags_file:atos-configurations/flags.optim.cfg \
+    --extra-arg=generator1=gen_rnd_uniform_deps,flags_files:atos-configurations/flags.optim.cfg \
     --extra-arg=stage_cookie=expl_first \
     --nbiters=5 --cookie=expl_second
 
+# random on all flags
+$ROOT/bin/atos generator \
+    --generator=gen_explore_random \
+    --nbiters=5 --optim-levels=-O3 --optim-variants=base
 
+# double gen-staged
+$ROOT/bin/atos generator \
+    --generator=gen_chained \
+    --extra-arg=generator1=gen_base \
+    --extra-arg=generator2=gen_explore_inline \
+    --extra-arg=generator3=gen_explore_loop \
+    --extra-arg=generator4=gen_explore_optim \
+    --extra-arg=generator5=gen_explore_inline \
+    --extra-arg=generator6=gen_explore_loop \
+    --extra-arg=generator7=gen_explore_optim \
+    --nbiters=2 --optim-levels=-O3 --optim-variants=base --tradeoffs=5

602ed1e6b68d085ed3ceedaca7cc827102ee14b8
Christophe Guillon <christophe.guillon@st.com>
2013-04-18 10:31:59 +0200

    test081.sh: compile test in -fpic mode.
    The -fpic option was not provided in the test build.
    This is mandatory for some gcc installs.

--- tests/test081.sh
+++ tests/test081.sh
@@ -8,7 +8,7 @@ TEST_CASE="ATOS shared/exe link order"
 
 cp -a $SRCDIR/tests/resources/build_install_so_1/* .
 
-# Build an verify whether we can have a build with several
+# Build and verify whether we can have a build with several
 # link commands sharing a shared object of the same name.
 $ROOT/bin/atos-init -c -b "make clean all install" -r ./main.exe -e "main.exe libtest.so"
 mv main.exe main.exe.ref

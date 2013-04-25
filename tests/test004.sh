aa9327c956fb55a0664f8c84fbecf70a2bcc5b05
Christophe Guillon <christophe.guillon@st.com>
2013-04-25 16:44:24 +0200

    atos-deps: allow selection of targets to be built.
    The atos-configurations/targets file can be modified to remove
    or comment out (with '#') some targets determined by
    atos-init or atos-deps --all.
    In this case atos-deps must be run again with no arguments to
    reconstruct the build informations.
    This allow to remove useless targets, or targets that should not
    be optimized or targets that fail to rebuild with atos-build.
    
    Implements: http://hostname/tracker/?func=detail&aid=207974

--- tests/test004.sh
+++ tests/test004.sh
@@ -8,10 +8,10 @@ TEST_CASE="ATOS deps target/-l/-a"
 
 $ROOT/bin/atos-audit gcc -o sha1-c $SRCDIR/examples/sha1-c/sha.c $SRCDIR/examples/sha1-c/sha1.c
 $ROOT/bin/atos-deps sha1-c
-[ -f atos-configurations/targets -a `wc -l <atos-configurations/targets` = 1 ]
+[ -f atos-configurations/targets -a `grep -v '^#' <atos-configurations/targets | wc -l` = 1 ]
 [ -f atos-configurations/objects ]
 [ -f atos-configurations/build.mk ]
-targets=`cat atos-configurations/targets`
+targets=`cat atos-configurations/targets | grep -v '^#'`
 [ "$targets" = $PWD/sha1-c ]
 objects=`cat atos-configurations/objects`
 [ "$objects" = "" ] # no objects as it is a link command
@@ -21,7 +21,7 @@ cmp sha1-c sha1-c.ref
 
 mv sha1-c sha1-c.ref
 $ROOT/bin/atos-deps -l
-[ -f atos-configurations/targets -a `wc -l <atos-configurations/targets` = 1 ]
+[ -f atos-configurations/targets -a `grep -v '^#' <atos-configurations/targets | wc -l` = 1 ]
 [ -f atos-configurations/objects ]
 [ -f atos-configurations/build.mk ]
 make -f atos-configurations/build.mk $PWD/sha1-c
@@ -32,10 +32,10 @@ cd sha1
 make clean
 $ROOT/bin/atos-audit make all
 $ROOT/bin/atos-deps -a
-targets=`cat atos-configurations/targets`
-[ "$targets" != "" ] # check that some targets was sen
+targets=`cat atos-configurations/targets | grep -v '^#'`
+[ "$targets" != "" ] # check that some targets were seen
 objects=`cat atos-configurations/objects`
-[ "$objects" != "" ] # check that some objects was sen
+[ "$objects" != "" ] # check that some objects were seen
 mv sha sha.ref
 mv shacmp shacmp.ref
 mv shatest shatest.ref

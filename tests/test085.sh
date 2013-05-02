68c7ba4fa2d4738ee51696d863a81da0a5c41a32
Antoine MOYNAULT <antoine.moynault@st.com>
2013-05-02 17:48:23 +0200

    tests: fixed test085 (too long).

--- tests/test085.sh
+++ tests/test085.sh
@@ -23,23 +23,22 @@ $ROOT/bin/atos-opt -r -a "-O3 --param inline-unit-growth=60 -finline-small-funct
 $ROOT/bin/atos generator --generator=gen_flag_values \
     --extra-arg=variant_id=OPT-O3--paraminline-unit-growth=60-finline-small-functions
 
+# run only in interactive mode if LONG_TESTS is set
+if [ "$LONG_TESTS" == "1" -a "$MAKELEVEL" == "" ]; then
 
-
-$ROOT/bin/atos-opt -r -f -l -a "-O3 --param inline-unit-growth=60 --param max-inline-insns-recursive=600 --param max-inline-insns-single=1200 --param max-inline-recursive-depth-auto=4 --param max-inline-recursive-depth=10 --param partial-inlining-entry-probability=0"
-
-$ROOT/bin/atos generator --generator=gen_flag_values \
-    --extra-arg=variant_id=OPT-fprofile-use-O3--paraminline-unit-growth=60--parammax-inline-insns-recursive=600--parammax-inline-insns-single=1200--parammax-inline-recursive-depth-auto=4--parammax-inline-recursive-depth=10--parampartial-inlining-entry-probability=0-O3--paraminline-unit-growth=60--parammax-inline-insns-recursive=600--parammax-inline-insns-single=1200--parammax-inline-recursive-depth-auto=4--parammax-inline-recursive-depth=10--parampartial-inlining-entry-probability=0-flto
-
-
-
-$ROOT/bin/atos-opt -r -l -a "-O3 --param inline-unit-growth=40 --param large-function-growth=200 --param large-stack-frame-growth=4000 --param large-stack-frame=250 --param large-unit-insns=34000 --param max-inline-insns-recursive=800 --param max-inline-recursive-depth-auto=4 --param max-inline-recursive-depth=8 --param min-inline-recursive-probability=95 --param partial-inlining-entry-probability=70 -findirect-inlining -finline-functions -fno-inline-functions-called-once -finline-small-functions -fno-partial-inlining"
-
-$ROOT/bin/atos generator --generator=gen_flag_values \
-    --extra-arg=variant_id=OPT-O3--paraminline-unit-growth=40--paramlarge-function-growth=200--paramlarge-stack-frame-growth=4000--paramlarge-stack-frame=250--paramlarge-unit-insns=34000--parammax-inline-insns-recursive=800--parammax-inline-recursive-depth-auto=4--parammax-inline-recursive-depth=8--parammin-inline-recursive-probability=95--parampartial-inlining-entry-probability=70-findirect-inlining-finline-functions-fno-inline-functions-called-once-finline-small-functions-fno-partial-inlining-flto
-
-
-
-$ROOT/bin/atos-opt -r -l -a "-Os --param large-function-growth=400 --param large-function-insns=1500 --param max-inline-insns-recursive=700 --param max-inline-insns-single=1600 --param max-inline-recursive-depth-auto=12 --param partial-inlining-entry-probability=90 -fno-early-inlining -fno-inline-functions -finline-functions-called-once"
-
-$ROOT/bin/atos generator --generator=gen_flag_values \
-    --extra-arg=variant_id=OPT-Os--paramlarge-function-growth=400--paramlarge-function-insns=1500--parammax-inline-insns-recursive=700--parammax-inline-insns-single=1600--parammax-inline-recursive-depth-auto=12--parampartial-inlining-entry-probability=90-fno-early-inlining-fno-inline-functions-finline-functions-called-once-flto
+    $ROOT/bin/atos-opt -r -f -l -a "-O3 --param inline-unit-growth=60 --param max-inline-insns-recursive=600 --param max-inline-insns-single=1200 --param max-inline-recursive-depth-auto=4 --param max-inline-recursive-depth=10 --param partial-inlining-entry-probability=0"
+    
+    $ROOT/bin/atos generator --generator=gen_flag_values \
+	--extra-arg=variant_id=OPT-fprofile-use-O3--paraminline-unit-growth=60--parammax-inline-insns-recursive=600--parammax-inline-insns-single=1200--parammax-inline-recursive-depth-auto=4--parammax-inline-recursive-depth=10--parampartial-inlining-entry-probability=0-O3--paraminline-unit-growth=60--parammax-inline-insns-recursive=600--parammax-inline-insns-single=1200--parammax-inline-recursive-depth-auto=4--parammax-inline-recursive-depth=10--parampartial-inlining-entry-probability=0-flto
+    
+    $ROOT/bin/atos-opt -r -l -a "-O3 --param inline-unit-growth=40 --param large-function-growth=200 --param large-stack-frame-growth=4000 --param large-stack-frame=250 --param large-unit-insns=34000 --param max-inline-insns-recursive=800 --param max-inline-recursive-depth-auto=4 --param max-inline-recursive-depth=8 --param min-inline-recursive-probability=95 --param partial-inlining-entry-probability=70 -findirect-inlining -finline-functions -fno-inline-functions-called-once -finline-small-functions -fno-partial-inlining"
+    
+    $ROOT/bin/atos generator --generator=gen_flag_values \
+	--extra-arg=variant_id=OPT-O3--paraminline-unit-growth=40--paramlarge-function-growth=200--paramlarge-stack-frame-growth=4000--paramlarge-stack-frame=250--paramlarge-unit-insns=34000--parammax-inline-insns-recursive=800--parammax-inline-recursive-depth-auto=4--parammax-inline-recursive-depth=8--parammin-inline-recursive-probability=95--parampartial-inlining-entry-probability=70-findirect-inlining-finline-functions-fno-inline-functions-called-once-finline-small-functions-fno-partial-inlining-flto
+    
+    $ROOT/bin/atos-opt -r -l -a "-Os --param large-function-growth=400 --param large-function-insns=1500 --param max-inline-insns-recursive=700 --param max-inline-insns-single=1600 --param max-inline-recursive-depth-auto=12 --param partial-inlining-entry-probability=90 -fno-early-inlining -fno-inline-functions -finline-functions-called-once"
+    
+    $ROOT/bin/atos generator --generator=gen_flag_values \
+	--extra-arg=variant_id=OPT-Os--paramlarge-function-growth=400--paramlarge-function-insns=1500--parammax-inline-insns-recursive=700--parammax-inline-insns-single=1600--parammax-inline-recursive-depth-auto=12--parampartial-inlining-entry-probability=90-fno-early-inlining-fno-inline-functions-finline-functions-called-once-flto
+    
+fi

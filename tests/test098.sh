cdd55a7bf7412aeb49fd3840f674d41c93ac7882
Christophe Guillon <christophe.guillon@st.com>
2013-05-17 15:46:15 +0200

    atos-driver.py: support overwritting of default logger output fd.
    By default logger outputs to the sys.stderr fd, added option
    --atos-debug-fd to redirect it to any fd number.
    In atos audit and atos compile we dup sys.stderr before invoking
    atos-driver.
    This fixes transparency issues in audit and build --force where
    atos logs were mixed with standard build system stderr.
    
    Fixes: http://hostname/tracker/?func=detail&aid=210603

---
+++ tests/test098.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="atos-driver debug options"
+
+rm -f log*
+# Check that no spurious output is emmited on stderr
+$ROOT/bin/atos-driver gcc 2>log2 || true
+head -1 log2 | grep "no input file" >/dev/null
+[ $? = 0 ] || exit 1
+
+rm -f log*
+# Check that debug mode outputs to stderr by default
+$ROOT/bin/atos-driver --atos-debug gcc 2>log2 || true
+head -1 log2 | grep "DEBUG:" >/dev/null
+[ $? = 0 ] || exit 1
+head -2 log2 | tail -1 | grep "no input file" >/dev/null
+[ $? = 0 ] || exit 1
+
+rm -f log*
+# Check that debug is redirected to fd 3 when requested
+$ROOT/bin/atos-driver --atos-debug --atos-debug-fd=3 gcc 3>log3 2>log2 || true
+head -1 log3 | grep "DEBUG:" >/dev/null
+[ $? = 0 ] || exit 1
+head -1 log2 | grep "no input file" >/dev/null
+[ $? = 0 ] || exit 1
+
+rm -f log*
+# Check that debug is redirected to fd 3 and log file when requested
+$ROOT/bin/atos-driver --atos-debug --atos-debug-fd=3 --atos-log-file=log4 gcc 3>log3 2>log2 || true
+head -1 log4 | grep "DEBUG:" >/dev/null
+[ $? = 0 ] || exit 1
+head -1 log3 | grep "DEBUG:" >/dev/null
+[ $? = 0 ] || exit 1
+head -1 log2 | grep "no input file" >/dev/null
+[ $? = 0 ] || exit 1

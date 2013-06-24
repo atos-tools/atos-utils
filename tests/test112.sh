27564e14b44cad3ca114d53e58d0d971a584f671
Christophe Guillon <christophe.guillon@st.com>
2013-06-24 11:05:18 +0200

    obj_storage: added test for incorrect handling of links.
    When an input file is a soft link, the stored blob must
    be the actual dereferenced content, not the link itself.
    This issue has been fixed with the removal of hard links
    from source file to object storage.
    Added a specific test for this additional use case.
    
    Refs: http://codex.cro.st.com/tracker/?func=detail&aid=219386
    Fixes: http://codex.cro.st.com/tracker/?func=detail&aid=218748

---
+++ tests/test112.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+#
+
+source `dirname $0`/common.sh
+
+TEST_CASE="ATOS storage of soft links"
+
+# Generate a test case that takes a previously
+# created link to an object (link-sha.o).
+# If the ATOS object storage does not
+# correctly handle link to input files
+# the rebuild may fail.
+# Actually the dereferenced linked file must be fully
+# copied to the storage, not the link itself.
+cat > build.sh <<EOF
+set -e
+gcc -c $SRCDIR/examples/sha1-c/sha1.c -o sha1.o
+gcc -o sha1-c sha1.o link-sha.o
+EOF
+
+# Create an object and a link out of atos-init
+gcc -c $SRCDIR/examples/sha1-c/sha.c -o sha.o
+ln -fs sha.o link-sha.o
+
+$ROOT/bin/atos-init -b "sh build.sh" -r "echo user 1" --debug
+
+# Remove all objects.
+# If the link was not correctly handled, the build will fail
+# with a link-sha.o: no such file or directory error.
+rm -f *.o
+$ROOT/bin/atos-build
+
+
+
+
+
+
